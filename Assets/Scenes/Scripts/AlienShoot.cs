97de90236f3cb20fdd2860c51cd7cfc1ac843c62
alvarohv2021 <alvarohv2013@gmail.com>
2024-02-23 20:11:03 +0100

    Probando cosas sobre las barreras

--- Assets/Scenes/Scripts/AlienShoot.cs
+++ Assets/Scenes/Scripts/AlienShoot.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class AlienShoot : MonoBehaviour
 {
     [SerializeField] float _bulletSpeed = 10f;
+    Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D colider)
     {
-        Destroy(gameObject);
+        _animator.SetTrigger("hit");
+        _bulletSpeed = 0f;
     }
 
     public void Spawn(Vector2 spawnPosition)
     {
         GameObject bullet = Instantiate(gameObject, new Vector3(spawnPosition.x, spawnPosition.y - 0.5f, 0f), Quaternion.identity);
+
     }
 
     void Update()
